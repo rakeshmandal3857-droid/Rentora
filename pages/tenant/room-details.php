e446564915edf7247c0c01be1f13751e902ed5d3
Rakesh <rakeshmandal3857@gmail.com>
2025-12-10 00:33:49 +0530

    update room detail page

--- pages/tenant/room-details.php
+++ pages/tenant/room-details.php
@@ -39,15 +39,12 @@ include __DIR__ . '/../../Components/header.php';
             <div class="desc">Experience refined living with our premium PG & Mess, where modern interiors meet homely comfort. Designed for students and professionals, the property offers hygienic meals, elegant rooms, and peaceful study spaces—all crafted to elevate your everyday stay. Ideally located near key business hubs and educational centers, it ensures effortless accessibility and a secure, well-managed environment. With attentive service, nutritious dining, and thoughtfully curated amenities, our PG delivers a truly comfortable, convenient, and upscale living experience.</div>
         </div>
     </div>
-</div>
 
-<section class="amenities-section">
     <div class="heading">Amenities :</div>
     <div class="amenities">
         <div class="amenity">
             <i class="fa-solid fa-wifi"></i> Free Wi-Fi
         </div>
-        
         <div class="amenity">
             <i class="fa-solid fa-wifi"></i> Free Wi-Fi
         </div>
@@ -77,6 +74,82 @@ include __DIR__ . '/../../Components/header.php';
         </div>
 
     </div>
+</div>
+
+<section class="section room-details-section">
+    <div class="heading">Choose your room</div>
+
+    <ul>
+        <li>
+            <div class="rooms">
+                <div class="room-details">
+                    <div class="heading">Room No. 1</div>
+                    <div class="desc">Room size : 100 sqft approx</div>
+
+                    <div class="room-tags">
+                        <span><i class="fa-solid fa-bed"></i> 2 Beds</span>
+                        <span><i class="fa-solid fa-bath"></i> 1 Bathroom</span>
+                        <span><i class="fa-solid fa-sink"></i> 1 kitchen</span>
+                    </div>
+                    <p class="room-update">*Only one bed is available</p>
+                </div>
+                
+                <div class="price">
+                    ₹ 1200/-
+                    <p>+ Electricity bill</p>
+                    <p>(per bed/month)</p>
+                </div>
+            </div>
+            <img src="../../assets/images/demo-room-img.jpg" alt="room-img">
+        </li>
+        <li>
+            <div class="rooms">
+                <div class="room-details">
+                    <div class="heading">Room No. 2</div>
+                    <div class="desc">Room size : 120 sqft approx</div>
+
+                    <div class="room-tags">
+                        <span><i class="fa-solid fa-bed"></i> 2 Beds</span>
+                        <span><i class="fa-solid fa-bath"></i> 1 Bathroom</span>
+                        <span><i class="fa-solid fa-sink"></i> 1 kitchen</span>
+                    </div>
+                </div>
+                
+                <div class="price">
+                    ₹ 1400/-
+                    <p>+ Electricity bill</p>
+                    <p>(per bed/month)</p>
+                </div>
+            </div>
+            <img src="../../assets/images/demo-room-img.jpg" alt="room-img">
+        </li>
+        <li>
+            <div class="rooms">
+                <div class="room-details">
+                    <div class="heading">Room No. 3</div>
+                    <div class="desc">Room size : 150 sqft approx</div>
+
+                    <div class="room-tags">
+                        <span><i class="fa-solid fa-bed"></i> 2 Beds</span>
+                        <span><i class="fa-solid fa-bath"></i> 1 Bathroom</span>
+                        <span><i class="fa-solid fa-sink"></i> 1 kitchen</span>
+                    </div>
+                    <p class="room-update">*Only two beds are available</p>
+                </div>
+                
+                <div class="price">
+                    ₹ 1300/-
+                    <p>+ Electricity bill</p>
+                    <p>(per bed/month)</p>
+                </div>
+            </div>
+            <img src="../../assets/images/demo-room-img.jpg" alt="room-img">
+        </li>
+    </ul>
+
+    <a href="tel:+9547085012">
+        <button class="btn-primary">Call the Owner : +91 9547085012</button>
+    </a>
 </section>
 
 <script src="./room-details.js"></script>
